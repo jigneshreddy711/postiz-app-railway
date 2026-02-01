51e7d3bfab8948ab406ad50f6719750fcfbf5012
nithesh.b <Nithesh.Battepati@kalibr8.io>
2026-02-01 19:00:05 +0530

    206

--- libraries/nestjs-libraries/src/openai/extract.content.service.ts
+++ libraries/nestjs-libraries/src/openai/extract.content.service.ts
@@ -19,7 +19,7 @@ export class ExtractContentService {
     const dom = new JSDOM(load);
 
     // only element that has a title
-    const allTitles = Array.from(dom.window.document.querySelectorAll('*')) as unknown as Element[]
+    const allTitles = (Array.from(dom.window.document.querySelectorAll('*')) as unknown as Element[])
       .filter((f) => {
         return (
           f.querySelector('h1') ||
